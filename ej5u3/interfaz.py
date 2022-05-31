cf8a29454df9461433fe108432551b1f1fba00df
Babsia <63377866+Babsia@users.noreply.github.com>
2022-05-31 16:13:30 -0300

    Add files via upload

---
+++ ej5u3/interfaz.py
@@ -0,0 +1,10 @@
+from zope.interface import Interface
+from zope.interface import implementer
+
+class interfaz(Interface):
+    def insertarelemento(elemento,pos):
+        pass
+    def agregarelemento(elemento):
+        pass
+    def mostrarelemento(elemento):
+        pass
